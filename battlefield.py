140d121c110a9c4ffd2d2bf3b045e52a0a346e77
jloescher <jonathan@jloescher.com>
2023-01-05 16:56:37 -0500

    Updated battlefield class to handle more than 2 participants Created weapon class

--- battlefield.py
+++ battlefield.py
@@ -1,4 +1,22 @@
+import random
+
+
 class Battlefield:
-    def __init__(self, robot, dinosaur):
-        self.robot = robot
-        self.dinosaur = dinosaur
+    def __init__(self, participants):
+        self.participants = []
+
+    def add_participants(self, *participants):
+        self.participants.extend(participants)
+
+    def start_battle(self):
+        while any(p.health > 0 for p in self.participants):
+            for attacker in self.participants:
+                if attacker.health <= 0:
+                    continue
+                targets = [
+                    p for p in self.participants if p.health > 0 and p != attacker
+                ]
+                if not targets:
+                    break
+                target = random.choice(targets)
+                attacker.attack(target)
